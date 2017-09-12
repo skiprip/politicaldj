842ff11e06c7fe7c44e83bdca4498e3703408c30
Skip <31902419+skiprip@users.noreply.github.com>
2017-09-12 16:54:11 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -25,15 +25,15 @@
         }
 
         // Example code for a bot command:
-        bot.commands.baconCommand = {
-          command: 'bacon',  // The command to be called. With the standard command literal this would be: !bacon
+        bot.commands.pingCommand = {
+          command: 'ping',  // The command to be called. With the standard command literal this would be: !bacon
           rank: 'user', // Minimum user permission to use the command
           type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
           functionality: function (chat, cmd) {
             if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
             if (!bot.commands.executable(this.rank, chat)) return void (0);
             else {
-              API.sendChat("/me Bacon!!!");
+              API.sendChat("/me [PolitiBOT]: PolitiBOT is Online! Do not worry!");
             }
           }
         };
@@ -46,7 +46,7 @@
     //Change the bots default settings and make sure they are loaded on launch
 
     localStorage.setItem("basicBotsettings", JSON.stringify({
-      botName: "basicBot",
+      botName: "PolitiBOT",
       language: "english",
       chatLink: "https://rawgit.com/basicBot/source/master/lang/en.json",
       scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
@@ -68,11 +68,11 @@
       maximumLocktime: 10,
       cycleGuard: true,
       maximumCycletime: 10,
-      voteSkip: false,
+      voteSkip: true,
       voteSkipLimit: 10,
       historySkip: false,
       timeGuard: true,
-      maximumSongLength: 10,
+      maximumSongLength: 8,
       autodisable: false,
       commandCooldown: 30,
       usercommandsEnabled: true,
@@ -106,9 +106,9 @@
       songstats: true,
       commandLiteral: "!",
       blacklists: {
-        NSFW: "https://rawgit.com/basicBot/custom/master/blacklists/NSFWlist.json",
-        OP: "https://rawgit.com/basicBot/custom/master/blacklists/OPlist.json",
-        BANNED: "https://rawgit.com/basicBot/custom/master/blacklists/BANNEDlist.json"
+        NSFW: "https://rawgit.com/skiprip/politicaldj/master/blacklists/BANNEDlist.json",
+        OP: "https://rawgit.com/skiprip/politicaldj/master/blacklists/OPlist.json",
+        BANNED: "https://rawgit.com/skiprip/politicaldj/master/blacklists/BANNEDlist.json"
       }
     }));
 
