b4f977711992de7ebc98b9e77dfeae376b024d6a
Skip <31902419+skiprip@users.noreply.github.com>
2017-09-12 20:18:32 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -34,17 +34,7 @@
             if (!bot.commands.executable(this.rank, chat)) return void (0);
             else {
               API.sendChat("/me [PolitiBOT]: PolitiBOT is Online! Do not worry!");
-        bot.commands.whatisCommand = {
-          command: 'wip',  // The command to be called. With the standard command literal this would be: !bacon
-          rank: 'user', // Minimum user permission to use the command
-          type: 'exact', // Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
-          functionality: function (chat, cmd) {
-            if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-            if (!bot.commands.executable(this.rank, chat)) return void (0);
-            else {
-              API.sendChat("/me Political! is a Plug.dj community for EDM, Rock and Political Styled music!, Check our info page at http://skip.rip/plug/info.html for more!");
             }
-           }
           }
         };
 
